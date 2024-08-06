041415d94edb78425f2eada36ff0e0de32b24f01
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-06 16:08:30 +0530

    add bot name

--- ChatBot/src/chatBot/cofig.jsx
+++ ChatBot/src/chatBot/cofig.jsx
@@ -2,6 +2,7 @@ import {createChatBotMessage} from 'react-chatbot-kit'
 
 const config={
     initialMessages:[createChatBotMessage(`Hello World`)],
+    botName:"AgeDriver"
 };
 
 export default config;
