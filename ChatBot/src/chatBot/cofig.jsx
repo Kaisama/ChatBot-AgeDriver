4d42801460e85b9df15efb79f2e84b5d26306bf8
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-07 11:55:41 +0530

    debug

--- ChatBot/src/chatBot/cofig.jsx
+++ ChatBot/src/chatBot/cofig.jsx
@@ -1,15 +1,29 @@
 import {createChatBotMessage} from 'react-chatbot-kit'
 import Avatar from './Avatar';
 import UserAvatar from './UserAvatar';
+import StartBtn from './StartBtn';
+import StartSlow from './StartSlow';
 
 const config={
     botName:"AgeDriver Explorer",
-    initialMessages:[createChatBotMessage(`Hi! I'm AgeDriver Explorer`)],
+    initialMessages:[createChatBotMessage(`Hi! I'm AgeDriver Explorer`,{
+        widget:"StartBtn"
+    })],
     customComponents:{
         botAvatar:(props)=><Avatar {...props}/>,
         userAvatar:(props)=><UserAvatar {...props}/>
 
-    }
+    },
+    widgets:[
+        {
+            widgetName:"StartBtn",
+            widgetFunc:(props)=> <StartBtn {...props}/>
+        },
+        {
+            widgetName:"StartSlow",
+            widgetFunc:(props)=> <StartSlow {...props}/>
+        }
+    ]
 };
 
 export default config;
