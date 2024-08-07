c904a59549db3826b1d369c1e907858238a82412
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-07 13:09:57 +0530

    and final result text after pressing the button

--- ChatBot/src/chatBot/StartSlow.jsx
+++ ChatBot/src/chatBot/StartSlow.jsx
@@ -1,9 +1,19 @@
 
-const StartSlow = () => {
+const StartSlow = (props) => {
+ 
+  console.log('props.state:', props.state);
+  const preference=(preference)=>{
+    const{name,age}=props.state.userData;
+
+    const category=props.state.userData.category;
+    const product =props.state.data[category][preference];
+    console.log(props.state.userData);
+    props.actions.finalResult(name,age,preference,product.name);
+  }
   return (
     <>
-    <button className="StartSlow slow">Slow</button>
-    <button className="StartFast">Fast</button>
+    <button className="StartSlow slow" onClick={()=>preference("slow")}>Slow</button>
+    <button className="StartFast" onClick={()=>preference("fast")}>Fast</button>
     </>
   )
 }
