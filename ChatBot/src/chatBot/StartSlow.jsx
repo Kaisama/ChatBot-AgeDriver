78083e900cad0cf26e93b1cc31435865be910462
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-07 11:56:30 +0530

    check

---
+++ ChatBot/src/chatBot/StartSlow.jsx
@@ -0,0 +1,11 @@
+
+const StartSlow = () => {
+  return (
+    <>
+    <button className="StartSlow slow">Slow</button>
+    <button className="StartFast">Fast</button>
+    </>
+  )
+}
+
+export default StartSlow
