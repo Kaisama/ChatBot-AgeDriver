23d5033871224461fef47316d85a9d82b289db0b
Akshat Bisht <bishtakshat64@gmail.com>
2024-08-07 11:55:24 +0530

    add after age and name message

--- ChatBot/src/chatBot/ActionProvider.jsx
+++ ChatBot/src/chatBot/ActionProvider.jsx
@@ -2,11 +2,41 @@ import { cloneElement } from "react"
 import { Children } from "react"
 
 const ActionProvider=({createChatBotMessage,setState,children})=>{
+
+    const afterAgeMessage=()=>{
+        const message= createChatBotMessage("Do you lean towards a fast anf thrilling ride or prefer a more relaxed and comfortable ride",{
+            widget:"StartSlow"
+        });
+        updateState(message)
+    }
+
+    const afterNameMessage=()=>{
+        const message =createChatBotMessage("Let me know your age so I can suggest you the best ride for you.")
+        updateState(message,"preference")
+    }
+
+
+    const initialAction=()=>{
+        const message=createChatBotMessage('Just type in your name to begin');
+        updateState(message , "age");
+    }
+
+    const updateState=(message , checker =" ")=>{
+        setState((prev)=>({
+            ...prev,
+            messages:[...prev.messages,message],
+            checker
+        }))
+    }
     return (
         <>
             {Children.map(children,(child)=>{
                 return cloneElement(child,{
-                    actions:{},
+                    actions:{
+                        initialAction,
+                        afterNameMessage,
+                        afterAgeMessage,
+                    },
                 });
             })}
         </>
